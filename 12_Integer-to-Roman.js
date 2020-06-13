4103201462056e6144e730996a748ee3bf15a130
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-06-13 22:34:40 +0900

    my solution to 12. Integer to Roman

---
+++ 12_Integer-to-Roman.js
@@ -0,0 +1,75 @@
+// 12. Integer to Roman
+
+var intToRoman = function (num) {
+  let chart = new Map([
+    ["1", "I"],
+    ["2", "II"],
+    ["3", "III"],
+    ["4", "IV"],
+    ["5", "V"],
+    ["6", "VI"],
+    ["7", "VII"],
+    ["8", "VIII"],
+    ["9", "IX"],
+    ["10", "X"],
+    ["20", "XX"],
+    ["30", "XXX"],
+    ["40", "XL"],
+    ["50", "L"],
+    ["60", "LX"],
+    ["70", "LXX"],
+    ["80", "LXXX"],
+    ["90", "XC"],
+    ["100", "C"],
+    ["200", "CC"],
+    ["300", "CCC"],
+    ["400", "CD"],
+    ["500", "D"],
+    ["600", "DC"],
+    ["700", "DCC"],
+    ["800", "DCCC"],
+    ["900", "CM"],
+    ["1000", "M"],
+    ["2000", "MM"],
+    ["3000", "MMM"],
+  ]);
+
+  let arrOutput = [];
+  let roman = [];
+  let result = "";
+  const sNumber = num.toString();
+
+  for (var i = 0, len = sNumber.length; i < len; i += 1) {
+    arrOutput.push(+sNumber.charAt(i));
+  }
+
+  let stringified = arrOutput.map((n) => `${n}`);
+
+  for (i = 0; i <= stringified.length; i++) {
+    if (i + 1 === stringified.length) {
+      roman.push(stringified[i]);
+    }
+    if (i + 2 === stringified.length) {
+      stringified[i] += "0";
+      roman.push(stringified[i]);
+    }
+    if (i + 3 === stringified.length) {
+      stringified[i] += "00";
+      roman.push(stringified[i]);
+    }
+    if (i + 4 === stringified.length) {
+      stringified[i] += "000";
+      roman.push(stringified[i]);
+    }
+  }
+
+  roman.forEach((n, index) => (n === "0" ? roman.splice(index, 1) : null));
+  roman.forEach((n, index) => (n === "00" ? roman.splice(index, 1) : null));
+  roman.forEach((n, index) => (n === "000" ? roman.splice(index, 1) : null));
+
+  for (let x of roman) {
+    result += chart.get(x);
+  }
+
+  return result;
+};
