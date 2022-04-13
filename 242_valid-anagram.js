ac2a6282d4f08cca4fa110f0f142ba84debeb952
Anthony Avila <anthony.p.avila@gmail.com>
2022-04-13 20:48:40 +0900

    correct prompt for Valid Anagram

--- 242_valid-anagram.js
+++ 242_valid-anagram.js
@@ -39,23 +39,18 @@ isAnagram(s, t);
 
 const prompt = `
 
-Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
-A subarray is a contiguous part of an array.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
  
 
 Example 1:
 
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
+Input: s = "anagram", t = "nagaram"
+Output: true
 Example 2:
 
-Input: nums = [1]
-Output: 1
-Example 3:
-
-Input: nums = [5,4,-1,7,8]
-Output: 23
+Input: s = "rat", t = "car"
+Output: false
 `;
