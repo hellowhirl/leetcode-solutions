1b4689e9bf3d12d6d8d3e9595a3d995a976bdc62
Anthony Avila <anthony.p.avila@gmail.com>
2022-07-10 09:37:56 +0900

    add config files

---
+++ 20-valid-parentheses.ts
@@ -0,0 +1,17 @@
+function isValid(s: string): boolean {
+  return true;
+}
+
+class Stack {
+  stack: string[];
+  currentIndex: number;
+
+  constructor() {
+    this.stack = [];
+    this.currentIndex = 0;
+  }
+
+  // push(val) {
+  //   this.stack[this.currentIndex] = val;
+  // }
+}
