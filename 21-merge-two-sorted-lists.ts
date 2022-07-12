4fe6da9da6aed15d2551eeb16ccdfa3bc2dadf55
Anthony Avila <anthony.p.avila@gmail.com>
2022-07-12 14:11:10 +0900

    ts solution to Merge two sorted lists

--- 21-merge-two-sorted-lists.ts
+++ 21-merge-two-sorted-lists.ts
@@ -11,5 +11,25 @@ function mergeTwoLists(
   list1: ListNode | null,
   list2: ListNode | null
 ): ListNode | null {
-  return null;
+  let dummy = new ListNode(); // default to 0
+  const head = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2!.val) {
+      dummy.next = list1;
+      list1 = list1.next;
+    } else {
+      dummy.next = list2;
+      list2 = list2.next;
+    }
+    dummy = dummy.next; // move up dummy list node
+  }
+
+  // point dummy's next list node to beginning of other list node
+  if (list1 === null) dummy.next = list2;
+  else {
+    dummy.next = list1;
+  }
+
+  return head.next; // a reference to our beginning dummy list node
 }
