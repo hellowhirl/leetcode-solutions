cc31090f7435198d9a3b8816449c4f5d312d80f5
Anthony Avila <anthony.p.avila@gmail.com>
2022-07-19 23:43:52 +0900

    my solution for Length of last word

---
+++ 58-length-of-last-word.ts
@@ -0,0 +1,39 @@
+// 58. Length of last word
+
+function lengthOfLastWord(s: string): number {
+  let wordCount = 0;
+  let lastCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== " ") {
+      wordCount++;
+      lastCount = wordCount;
+    } else {
+      wordCount = 0;
+    }
+  }
+  return lastCount;
+}
+
+const problem58 = `
+
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+`;
