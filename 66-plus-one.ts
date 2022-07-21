ef5240c3c1946357ae6d4818c29cc9bab942eaea
Anthony Avila <anthony.p.avila@gmail.com>
2022-07-22 02:09:20 +0900

    refactor Plus one with one less if statement

--- 66-plus-one.ts
+++ 66-plus-one.ts
@@ -3,17 +3,13 @@
 function plusOne(digits: number[]): number[] {
   for (let i = digits.length - 1; i >= 0; i--) {
     if (digits[i] === 9) {
-      if (i === 0) {
-        digits[i] = 0;
-        digits.unshift(1);
-        break;
-      }
       digits[i] = 0;
     } else {
       digits[i]++;
-      break;
+      return digits;
     }
   }
+  digits.unshift(1);
   return digits;
 }
 
