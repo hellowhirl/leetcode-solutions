63004ae5fde74dc60c3a9ddadd7d919b8aa7917c
Anthony Avila <anthony.p.avila@gmail.com>
2023-05-14 19:35:18 +0900

    simplify my Pascals Triangle solution

--- 118-pascals-triangle.js
+++ 118-pascals-triangle.js
@@ -3,26 +3,22 @@
 //     if (numRows === 1) {
 //         return [[1]];
 //     }
-//     if (numRows === 2) {
-//         return [[1], [1, 1]];
-//     }
-//     var result = [[1], [1, 1]];
-//     numRows = numRows - 2;
-//     var currentRow = 2;
-//     while (numRows > 0) {
+//     var triangle = [[1]];
+//     var prevRowIndex = 0;
+//     while (numRows > 1) {
 //         var rowArr = [1];
 //         var pointer1 = 0;
 //         var pointer2 = 1;
-//         while (pointer2 < result[currentRow - 1].length) {
-//             rowArr.push(result[currentRow - 1][pointer1] + result[currentRow - 1][pointer2]);
+//         while (pointer2 < triangle[prevRowIndex].length) {
+//             rowArr.push(triangle[prevRowIndex][pointer1] + triangle[prevRowIndex][pointer2]);
 //             pointer1++;
 //             pointer2++;
 //         }
 //         rowArr.push(1);
 //         numRows--;
-//         currentRow++;
-//         result.push(rowArr);
+//         prevRowIndex++;
+//         triangle.push(rowArr);
 //     }
-//     return result;
+//     return triangle;
 // }
 // ;
--- 118-pascals-triangle.ts
+++ 118-pascals-triangle.ts
@@ -4,27 +4,23 @@ function generate(numRows: number): number[][] {
     if(numRows === 1) {
         return [[1]];
     }
-    if(numRows === 2) {
-        return [[1], [1, 1]];
-    }
     
-    const result = [[1], [1, 1]];
-    numRows = numRows - 2;
-    let currentRow = 2;
+    const triangle = [[1]];
+    let prevRowIndex = 0;
     
-    while(numRows > 0) {
+    while(numRows > 1) {
         const rowArr = [1];
         let pointer1 = 0;
         let pointer2 = 1;
-        while(pointer2 < result[currentRow - 1].length) {
-            rowArr.push(result[currentRow - 1][pointer1] + result[currentRow - 1][pointer2]);
+        while(pointer2 < triangle[prevRowIndex].length) {
+            rowArr.push(triangle[prevRowIndex][pointer1] + triangle[prevRowIndex][pointer2]);
             pointer1++;
             pointer2++;
         }
         rowArr.push(1);
         numRows--;
-        currentRow++;
-        result.push(rowArr);
+        prevRowIndex++;
+        triangle.push(rowArr);
     }
-    return result;
+    return triangle;
 };
