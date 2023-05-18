77b0afdcf3ba0f80bf02b6d3d425b55829e60856
Anthony Avila <anthony.p.avila@gmail.com>
2023-05-19 08:29:18 +0900

    assisted solution for Power of Three

---
+++ 326-power-of-three.ts
@@ -0,0 +1,12 @@
+// 326. Power of Three
+
+function isPowerOfThree(n: number): boolean {
+    if (n === 0) return false
+
+    while (n % 3 === 0) {
+        n /= 3
+    }
+
+    if (n === 1) return true
+    return false
+};
