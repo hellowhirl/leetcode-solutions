bbc51d4e9075042fdc0828a10bfcc632218a9ba4
Anthony Avila <anthony.p.avila@gmail.com>
2023-06-11 17:30:53 +0900

    some improvements to String to Integer (atoi)

--- 8-string-to-integer-atoi.ts
+++ 8-string-to-integer-atoi.ts
@@ -1,56 +1,34 @@
 // 8. String to Integer (atoi)
 
 function myAtoi(s: string): number {
-  debugger;
-
-  //   s = s.replace(/\s/g, "");
-
-  if (s.length === 1 && s.charCodeAt(0) === 45) {
-    return 0;
-  }
-  if (
-    (s.charCodeAt(0) !== 32 &&
-      s.charCodeAt(0) !== 45 &&
-      s.charCodeAt(0) !== 43 &&
-      s.charCodeAt(0) < 47) ||
-    s.charCodeAt(0) > 57 ||
-    s.length === 0
-  ) {
-    return 0;
-  }
-
   let numberResult: string[] = [];
   let negative = false;
-
   let i = 0;
 
   while (s.charCodeAt(i) === 32) {
     i++;
   }
-  let start = i;
+  let startIndex = i;
 
   // -
-  if (s.charCodeAt(start) === 45) {
+  if (s.charCodeAt(startIndex) === 45) {
     negative = true;
-    start++;
+    startIndex++;
   }
   // +
-  else if (s.charCodeAt(start) === 43) {
+  else if (s.charCodeAt(startIndex) === 43) {
     negative = false;
-    start++;
+    startIndex++;
   }
 
-  for (let i = start; i < s.length; i++) {
+  for (let i = startIndex; i < s.length; i++) {
     if (
       s.charCodeAt(i) < 47 &&
       s.charCodeAt(i) > 57 &&
       numberResult.length !== 0
     ) {
       break;
-    } else if (
-      (s.charCodeAt(i) >= 47 && s.charCodeAt(i) <= 57) ||
-      s.charCodeAt(i) === 46
-    ) {
+    } else if (s.charCodeAt(i) >= 47 && s.charCodeAt(i) <= 57) {
       numberResult.push(s[i]);
     } else {
       break;
@@ -64,11 +42,11 @@ function myAtoi(s: string): number {
   let finalNumber = parseInt(numberResult.join(""));
   finalNumber = negative ? -finalNumber : finalNumber;
 
-  if (finalNumber >= 2147483647 || finalNumber <= -2147483648 - 1) {
-    return negative ? -2147483648 : 2147483647;
+  if (finalNumber < -(2 ** 31) || finalNumber > 2 ** 31 - 1) {
+    return negative ? -(2 ** 31) : 2 ** 31 - 1;
   }
 
-  return Math.floor(finalNumber);
+  return finalNumber;
 }
 
 // const test8 = "-91283472332";
